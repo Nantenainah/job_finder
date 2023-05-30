52bae9df64d2d7b25b21ab3de0eeecf161018574
tambatra <tambatra00000000@gmail.com>
2023-05-30 21:14:19 +0200

    Implementing users feature basic

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,19 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const usersRoute = require('./users/route')
 
 require('dotenv').config({ path: './config/.env' })
 require('./config/db')
 
 // middleware
 
-
 // routes
-
+app.get('/', (req, res) => {
+	res.send('Hello world')
+})
+app.use('/users', usersRoute)
 
 // server listening
 app.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}`);
+	console.log(`Listening on port ${process.env.PORT}`)
 })
