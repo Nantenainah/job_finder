70545e6c1b9b9c3631618331ed1ff3e63c3f1247
tambatra <tambatra00000000@gmail.com>
2023-06-07 19:50:36 +0200

    Fixing merging problem

--- backend/posts/route.ts
+++ backend/posts/route.ts
@@ -1,43 +1,39 @@
-import express from "express"
-import Post from "./model"
+import express from 'express'
+import Post from './model'
 const router = express.Router()
 
 /**
  * Get one all posts
  */
 router.get('/', async (req, res) => {
-    const posts = await Post.find()
-    return res.json(posts)
+	const posts = await Post.find()
+	return res.json(posts)
 })
 
 /**
  * Get one post
  */
 router.get('/:postId', async (req, res) => {
-    try {
-        const post = await Post.findById(req.params.postId)
-        return res.json(post)
-    } catch {
-        return res.sendStatus(404)
-    }
+	try {
+		const post = await Post.findById(req.params.postId)
+		return res.json(post)
+	} catch {
+		return res.sendStatus(404)
+	}
 })
 
 /**
  * Create one post
  */
 router.post('/', async (req, res) => {
-    const post = new Post(req.body)
-    try {
-<<<<<<< HEAD
-        await post.validate
-=======
-        await post.validate()
->>>>>>> 1838c9f52d116937748f321f6705c559931c689d
-        post.save()
-        return res.json(post)
-    } catch {
-        return res.sendStatus(400)
-    }
+	const post = new Post(req.body)
+	try {
+		await post.validate()
+		await post.save()
+		return res.json(post)
+	} catch {
+		return res.sendStatus(400)
+	}
 })
 
 export default router
