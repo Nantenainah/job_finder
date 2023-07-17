2a35102385d43ab7bc72426cefb43bb0b7edee4c
Lafatriniaina <lafatraniaina8@gmail.com>
2023-07-17 20:08:34 +0200

    Modify main and App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
-import { RouterProvider } from 'react-router-dom';
-import {ROUTES} from  './routes' 
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Register from './components/users/UserRegister';
+import Home from './components/Home/home';
+import UserLogin from './components/users/UserLogin';
 
 const App = () => {
   return (
     <React.StrictMode>
-    <RouterProvider router={ROUTES} />
-  </React.StrictMode>
+      <Router>
+        <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route index path="/register" element={<Register />} />
+          <Route index path="/login" element={<UserLogin />} />
+        </Routes>
+      </Router>
+    </React.StrictMode>
+    
   )
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,9 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './styles/user.scss'
-import './index.scss'
-import Register from './components/users/UserRegister';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route index path="/" element={<App />} />
-        <Route index path="/register" element={<Register />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 )
