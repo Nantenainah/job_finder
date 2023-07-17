b0abbb14bb9632c4fd888d2b4d701aa2b5efd1cc
Lafatriniaina <lafatraniaina8@gmail.com>
2023-07-17 20:18:13 +0200

    Header bar

---
+++ frontend/src/Fragments/Home/Header.tsx
@@ -0,0 +1,33 @@
+import { ChangeEvent, FunctionComponent, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import '../../styles/home.scss';
+import logo  from "../../assets/logo_ispm.png";
+
+export const HomeHeader: FunctionComponent = () => {
+
+    const [searchTerm, setSearchTerm] = useState("");
+      
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value);
+    }
+    return (
+            <div className="header">
+                <div className="fusion">
+                    <div>
+                        <img src={logo} alt="logo" className="header-logo" />
+                        <p>Job Finder</p>
+                    </div>
+                    <div className="search">
+                        <input
+                            type="search"
+                            placeholder="Search..."
+                            value={searchTerm}
+                            onChange={handleChange}
+                        />
+                        <FontAwesomeIcon icon={faSearch} className="search-icon" />
+                    </div>
+                </div>
+            </div>
+    );
+}
