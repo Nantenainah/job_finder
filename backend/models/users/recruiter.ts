22cbf85db965ae973ac7f713ceb95e32aea158d2
ye-tambatra <nofiniaina.tambatra@gmail.com>
2023-09-03 21:06:14 -0500

    Updating the recruiter model

--- backend/models/users/recruiter.ts
+++ backend/models/users/recruiter.ts
@@ -1,22 +1,30 @@
 import { Document, Schema, model } from "mongoose";
-import { IUser } from "./user";
 
-export interface IRecruiter extends Document {
-    user: Schema.Types.ObjectId | IUser;
-    companyName: string;
-    phoneNumber: string;
+export interface IRecruiterBase extends Document {
     email: string;
-    companyDescription: string;
+    username: string;
+    lastName: string;
+    firstName: string;
+    password: string;
+}
+
+export interface IRecruiter extends IRecruiterBase {
+    companyName?: string;
+    phoneNumber?: string;
+    companyDescription?: string;
     websiteUrl?: string;
     logo?: string;
 }
 
 const recruiterSchema = new Schema<IRecruiter>({
-    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    companyName: { type: String, required: true },
-    phoneNumber: { type: String, required: true },
-    email: { type: String, required: true },
-    companyDescription: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true },
+    lastName: { type: String, required: true },
+    firstName: { type: String, required: true },
+    password: { type: String, required: true },
+    companyName: { type: String },
+    phoneNumber: { type: String },
+    companyDescription: { type: String },
     websiteUrl: { type: String },
     logo: { type: String },
 });
