873d811a0c165bb0665b03fec47ca5549f136715
ye-tambatra <nofiniaina.tambatra@gmail.com>
2023-09-03 21:47:08 -0500

    Adding the job listing on the recruiter's route

--- backend/routes/recruiter.ts
+++ backend/routes/recruiter.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import Recruiter from "../models/recruiter";
+import JobListing from "../models/job-listing";
 
 const router = express.Router();
 
@@ -62,6 +63,43 @@ router.delete("/:id", async (req, res) => {
         res.json(deletedRecruiter);
     } catch (error: any) {
         res.status(500).json({ error: error.message });
+    }
+});
+
+router.get("/:id/job-listings", async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const recruiter = await Recruiter.findById(id);
+        if (!recruiter) {
+            return res.status(404).json({ message: "Recruiter not found" });
+        }
+
+        const jobListings = await JobListing.find({ recruiter: id });
+        res.json(jobListings);
+    } catch (error: any) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+router.post("/:id/job-listings", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const jobListingData = req.body;
+
+        const recruiter = await Recruiter.findById(id);
+        if (!recruiter) {
+            return res.status(404).json({ message: "Recruiter not found" });
+        }
+
+        jobListingData.recruiter = id;
+
+        const newJobListing = new JobListing(jobListingData);
+        await newJobListing.save();
+
+        res.status(201).json(newJobListing);
+    } catch (error: any) {
+        res.status(400).json({ error: error.message });
     }
 });
 
