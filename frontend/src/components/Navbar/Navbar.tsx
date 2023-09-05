d5ab27f62806025230ba10f8d3d0e5f9083547c7
ye-tambatra <nofiniaina.tambatra@gmail.com>
2023-09-05 17:21:49 -0500

    Adding login button

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,8 +1,8 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import Container from "../Container/Container";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faUser } from "@fortawesome/free-solid-svg-icons";
 import brandLogo from "../../assets/brand.png";
 
 function DesktopNavLink(props: { children: React.ReactNode; to: string }) {
@@ -38,27 +38,37 @@ function Navbar() {
         <header className="bg-gray-100 py-5 md:py-0">
             <nav>
                 <Container>
-                    <div className="flex justify-between items-center md:justify-start">
-                        <NavLink to="/" className="flex items-center">
-                            <img
-                                src={brandLogo}
-                                className="mr-3"
-                                alt="Job finder brand icon"
-                                width={35}
-                                height={35}
-                            />
-                            <span className="text-lg font-semibold">
-                                Job-finder
-                            </span>
-                        </NavLink>
+                    <div className="flex justify-between items-center">
+                        <div className="flex">
+                            <NavLink to="/" className="flex items-center">
+                                <img
+                                    src={brandLogo}
+                                    className="mr-3"
+                                    alt="Job finder brand icon"
+                                    width={35}
+                                    height={35}
+                                />
+                                <span className="text-lg font-semibold">
+                                    Job-finder
+                                </span>
+                            </NavLink>
 
-                        {/* Desktop version */}
-                        <div className="hidden md:ml-10 md:flex space-x-5">
-                            <DesktopNavLink to="/">Emplois</DesktopNavLink>
-                            <DesktopNavLink to="/publish">
-                                Publier
-                            </DesktopNavLink>
+                            {/* Desktop version */}
+                            <div className="hidden md:ml-10 md:flex space-x-5">
+                                <DesktopNavLink to="/">Emplois</DesktopNavLink>
+                                <DesktopNavLink to="/publish">
+                                    Publier
+                                </DesktopNavLink>
+                            </div>
                         </div>
+
+                        <Link
+                            to={"/login"}
+                            className="hidden md:block px-4 py-2 border border-gray-500 text-gray-500 rounded-full transition duration-200 hover:border-gray-800 hover:text-gray-800"
+                        >
+                            <FontAwesomeIcon icon={faUser} className="mr-2" />
+                            Se connecter
+                        </Link>
 
                         <button
                             className="md:hidden"
@@ -73,6 +83,9 @@ function Navbar() {
                         <div className="mt-4 md:hidden flex flex-col space-y-3">
                             <MobileNavLink to="/">Emplois</MobileNavLink>
                             <MobileNavLink to="/publish">Publier</MobileNavLink>
+                            <MobileNavLink to="/login">
+                                Se connecter
+                            </MobileNavLink>
                         </div>
                     )}
                 </Container>
--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -9,7 +9,7 @@ function Layout() {
             <main>
                 <Outlet />
             </main>
-           <Login/>
+            {/* <Login/> */}
         </>
     );
 }
