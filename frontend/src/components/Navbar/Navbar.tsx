df1a91ca9de2f39b665289cc2fd4f1326d832668
ye-tambatra <nofiniaina.tambatra@gmail.com>
2023-09-08 15:13:30 -0500

    Setting up the job details page xd

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import { Link, NavLink } from 'react-router-dom';
-import { MENU_DATA } from '../../constants/Menu';
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { MENU_DATA } from "../../constants/Menu";
 import { BiMenuAltLeft } from "react-icons/bi";
 import { AiTwotoneBell } from "react-icons/ai";
 
@@ -9,20 +9,25 @@ type NavbarProps = {
 };
 
 const Navbar: React.FC<NavbarProps> = ({ isConnected }) => {
-
     const [isMenuClicked, setMenuClicked] = useState(false);
     const modifiedMenuData = [...MENU_DATA];
 
     // Si l'utilisateur est connecté, add "Mon profil"
-    (isConnected) && (modifiedMenuData.push({ path: '/profile', title: 'Mon profil' }))
+    isConnected &&
+        modifiedMenuData.push({ path: "/profile", title: "Mon profil" });
 
     return (
-        <header className='bg-lightColor flex justify-between gap-14 items-center 
-                border-b-[1px] py-[1rem] px-[2rem] md:px-[5rem]'>
-
-            <NavLink to={'/'} className="logo flex gap-2 items-center cursor-pointer">
+        <header
+            className="bg-lightColor flex justify-between gap-14 items-center 
+                border-b-[1px] py-[1rem] px-[2rem] md:px-[5rem]"
+        >
+            <NavLink
+                to={"/"}
+                className="logo flex gap-2 items-center cursor-pointer"
+            >
                 <img
-                    src="./logo.png" alt="logo JobFinder"
+                    src="./logo.png"
+                    alt="logo JobFinder"
                     height={10}
                     width={25}
                 />
@@ -31,65 +36,84 @@ const Navbar: React.FC<NavbarProps> = ({ isConnected }) => {
                 </h1>
             </NavLink>
 
-            <nav className={`flex flex-col items-center gap-6 text-[14px] bg-lightColor rounded-bl-lg overflow-hidden 
+            <nav
+                className={`flex flex-col items-center gap-6 text-[14px] bg-lightColor rounded-bl-lg overflow-hidden 
                 py-4 shadow-sm shadow-gray-100 absolute top-20 right-0 transition-all duration-300
-                ${(isMenuClicked) ? 'w-[50%]' : 'w-0'} md:w-fit md:static md:flex-row md:shadow-none`}>
-
+                ${
+                    isMenuClicked ? "w-[50%]" : "w-0"
+                } md:w-fit md:static md:flex-row md:shadow-none`}
+            >
                 {modifiedMenuData.map(({ path, title }) => (
                     <NavLink
+                        key={path + "-" + title}
                         to={path}
                         className={({ isActive }) =>
-                            `menuList ${isActive ? "text-blueColor" : "text-gray-600 hover:text-blueColor before:bg-blueColor"}
-                            ${(title == 'Mon profil') ? 'md:hidden' : ''}`}>
+                            `menuList ${
+                                isActive
+                                    ? "text-blueColor"
+                                    : "text-gray-600 hover:text-blueColor before:bg-blueColor"
+                            }
+                            ${title == "Mon profil" ? "md:hidden" : ""}`
+                        }
+                    >
                         {title}
                     </NavLink>
                 ))}
 
                 <NavLink
-                    to={'/login'}
-                    className={`${isConnected ? 'hidden' : ''} border-[1px] flex p-2 px-3 rounded-sm bg-blueColor 
-                    text-lightColor cursor-pointer transition duration-300 md:hidden`}>
+                    to={"/login"}
+                    className={`${
+                        isConnected ? "hidden" : ""
+                    } border-[1px] flex p-2 px-3 rounded-sm bg-blueColor 
+                    text-lightColor cursor-pointer transition duration-300 md:hidden`}
+                >
                     se connecter
                 </NavLink>
             </nav>
 
             {/* -------------------------Si l'utilisateur est c-------------------------*/}
-            {(isConnected) ?
-                <div className="profil text-darkColor text-[15px] items-center gap-4 ml-auto 
-                            hidden group:cursor-pointer md:flex">
+            {isConnected ? (
+                <div
+                    className="profil text-darkColor text-[15px] items-center gap-4 ml-auto 
+                            hidden group:cursor-pointer md:flex"
+                >
                     <img
                         src="./avatar.jpg"
                         alt="photo de .."
-                        className='rounded-[100%] h-[30px] w-[35px] cursor-pointer'
+                        className="rounded-[100%] h-[30px] w-[35px] cursor-pointer"
                     />
 
-                    <div className='border-2 border-gray-300 p-2 rounded-[100%] cursor-pointer'>
+                    <div className="border-2 border-gray-300 p-2 rounded-[100%] cursor-pointer">
                         <AiTwotoneBell size={18} />
                     </div>
                 </div>
-                :
+            ) : (
                 <div className="hidden text-textColor text-[15px] items-center gap-4 ml-auto md:flex">
-                    <Link className="border-[1px] border-gray-400 p-2 px-3 rounded-sm  hover:bg-blueColor 
+                    <Link
+                        className="border-[1px] border-gray-400 p-2 px-3 rounded-sm  hover:bg-blueColor 
                         hover:text-lightColor transition duration-300 animate-fadeIn"
-                        to={'/sign-up'}>
+                        to={"/sign-up"}
+                    >
                         Inscription
                     </Link>
-                    <Link className="border-[1px] border-gray-400  p-2 px-3 rounded-sm hover:bg-blueColor 
+                    <Link
+                        className="border-[1px] border-gray-400  p-2 px-3 rounded-sm hover:bg-blueColor 
                         hover:text-lightColor transition duration-300 animate-fadeIn"
-                        to={'/login'}>
+                        to={"/login"}
+                    >
                         se connecter
                     </Link>
                 </div>
-            }
+            )}
 
             <BiMenuAltLeft
                 size={35}
-                color='black'
-                className='cursor-pointer md:hidden'
-                onClick={() => setMenuClicked(!isMenuClicked)} />
+                color="black"
+                className="cursor-pointer md:hidden"
+                onClick={() => setMenuClicked(!isMenuClicked)}
+            />
+        </header>
+    );
+};
 
-        </header >
-    )
-}
-
-export default Navbar
+export default Navbar;
--- frontend/src/pages/JobDetails/JobDetails.tsx
+++ frontend/src/pages/JobDetails/JobDetails.tsx
@@ -1,5 +1,154 @@
+import Container from "../../components/Container/Container";
+
+const RoundedText = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <span className="mx-3 px-5 py-1 border border-gray-500 text-gray-500 rounded-full mt-3">
+            {children}
+        </span>
+    );
+};
+
+const RelatedJobs = () => {
+    return (
+        <aside className=" shadow-md bg-white order-2 lg:order-1 lg:w-1/4 p-5">
+            <h1 className="font-semibold mb-10">Emplois similaires</h1>
+            <div className="flex flex-col space-y-5">
+                {Array.from(new Array(7)).map((_, index) => {
+                    return (
+                        <div
+                            className="flex items-start"
+                            key={"related-jobs-" + index}
+                        >
+                            <img className="bg-gray-500 w-[45px] h-[45px] mr-5 mt-1" />
+                            <div className="flex-1">
+                                <h1 className="font-medium">
+                                    Directrice marketing
+                                </h1>
+                                <h2 className="text-sm text-gray-400">
+                                    Société e-varotra, il y a une heure
+                                </h2>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </aside>
+    );
+};
+
+const JobInformations = () => {
+    return (
+        <div className="p-5 lg:w-3/4 lg:border-r border-gray-500">
+            <div className="flex items-center">
+                <img className="bg-gray-500 w-[50px] h-[50px] mr-5" />
+                <div>
+                    <h1 className="text-lg font-medium">
+                        Directrice marketing
+                    </h1>
+                    <h2 className="text-gray-400">Société e-varotra</h2>
+                </div>
+            </div>
+            <hr className="my-5" />
+            <h1 className="font-semibold">Description du poste : </h1>
+            <p className="text-gray-500">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia
+                laboriosam harum, delectus corporis voluptates recusandae
+                laborum nemo provident ut libero aliquam, possimus nam
+                repellendus in, animi sapiente commodi quos! Excepturi?
+            </p>
+            <h1 className="font-semibold mt-5">Responsabilités : </h1>
+            <p className="text-gray-500">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia
+                laboriosam harum, delectus corporis voluptates recusandae
+                laborum nemo provident ut libero aliquam, possimus nam
+                repellendus in, animi sapiente commodi quos! Excepturi?
+            </p>
+            <p className="text-gray-500">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia
+                laboriosam harum, delectus corporis voluptates recusandae
+                laborum nemo provident ut libero aliquam, possimus nam
+                repellendus in, animi sapiente commodi quos! Excepturi?
+            </p>
+            <p className="text-gray-500">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia
+                laboriosam harum, delectus corporis voluptates recusandae
+                laborum nemo provident ut libero aliquam, possimus nam
+                repellendus in, animi sapiente commodi quos! Excepturi?
+            </p>
+            <p className="text-gray-500">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia
+                laboriosam harum, delectus corporis voluptates recusandae
+                laborum nemo provident ut libero aliquam, possimus nam
+                repellendus in, animi sapiente commodi quos! Excepturi?
+            </p>
+            <h1 className="font-semibold mt-5">Compétences requis : </h1>
+            <div className="flex flex-wrap items-center">
+                <RoundedText>Créativité</RoundedText>
+                <RoundedText>Communication</RoundedText>
+                <RoundedText>Analyse</RoundedText>
+                <RoundedText>Stratégie</RoundedText>
+            </div>
+        </div>
+    );
+};
+
+const RecruiterInformations = () => {
+    return (
+        <div className="p-5 lg:w-1/4 flex flex-col">
+            <div className="flex-1">
+                <h1 className="font-semibold">A propos du client</h1>
+                <div className="mt-5">
+                    <h2>Nom du client : </h2>
+                    <h3 className="text-gray-500">Société e-varotra</h3>
+                </div>
+                <div className="mt-5">
+                    <h2>Membre depuis : </h2>
+                    <h3 className="text-gray-500">12 Janvier 2017</h3>
+                </div>
+                <div className="mt-5">
+                    <h2>Adresse</h2>
+                    <h3 className="text-gray-500">
+                        Ampitatafika, Antananarivo
+                    </h3>
+                </div>
+                <hr className="my-5" />
+                <h1 className="font-semibold mb-5">Autres informations</h1>
+                <div>
+                    <h2 className="">Secteurs d'activités : </h2>
+                    <div className="mb-3 flex flex-wrap items-center justify-start">
+                        <RoundedText>Créativité</RoundedText>
+                        <RoundedText>Communication</RoundedText>
+                        <RoundedText>Analyse</RoundedText>
+                        <RoundedText>Stratégie</RoundedText>
+                    </div>
+                </div>
+            </div>
+            <hr className="my-5" />
+            <button className="w-full rounded-sm bg-blueColor text-white py-3">
+                Postuler
+            </button>
+            <button className="w-full rounded-sm border border-black py-3 mt-3">
+                Enregistrer
+            </button>
+        </div>
+    );
+};
+
 function JobDetails() {
-    return <div>JobDetails</div>;
+    return (
+        <Container>
+            <div className="flex flex-col lg:space-x-5 lg:flex-row my-10">
+                <RelatedJobs />
+                <div className="order-1 lg:order-2 lg:w-3/4 shadow-md bg-white">
+                    <div className="h-[60px] bg-gray-600 rounded-t-lg" />
+                    <div className="flex flex-col lg:flex-row">
+                        <JobInformations />
+                        <RecruiterInformations />
+                    </div>
+                </div>
+            </div>
+        </Container>
+    );
 }
 
 export default JobDetails;
