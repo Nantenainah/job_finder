6c2ff90e0815b9651f6bead13b30ccbb69b5f59b
OnjaFanS <onjarabemanantsimba@gmail.com>
2023-10-23 13:49:40 +0300

    Add JobListing file

--- backend/routes/job-listing.ts
+++ backend/routes/job-listing.ts
@@ -104,6 +104,48 @@ router.get("/stats",async (req,res)=>{
 
 });
 
+
+router.get("/job_type_chart", async (req, res) => {
+
+    try {
+ 
+        const { month, year } = req.query; // Get the month as a number from the query paramete
+        if (!month || !year) {
+            throw new Error();
+        }
+        // Convert the month to a JavaScript Date object
+        const startDate = new Date(+year, +month - 1, 1); // month is 0-based, so subtract 1
+        const endDate = new Date(+year, +month, 0, 23, 59, 59); // Last day of the month, 23:59
+        
+        // Create a query to find job listings within the specified month
+        const jobListings = await JobListing.find({
+          createdAt: {
+            $gte: startDate,
+            $lte: endDate,
+          }
+        });
+        
+        
+        const values = JOB_TYPES.map((type) => {
+            let count=0;
+            jobListings.forEach(job => {
+                if (type === job.type){
+                    count++;
+                }
+            })
+            return {
+                name: type,
+                value:count
+            }
+        })
+        res.json(values);
+    } catch (error: any) {
+        res.status(500).json({ error: error.message });
+    }
+   })
+
+
+
 router.get("/:id", async (req, res) => {
     try {
         const jobListing = await JobListing.findById(req.params.id);
@@ -115,17 +157,5 @@ router.get("/:id", async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 });
-
-
-
-
-
-
-
-
-
-
-
-
 
 export default router;
