6a157b1860eac60a509ad93242545c41677c3889
ye-tambatra <nofiniaina.tambatra@gmail.com>
2023-10-24 21:20:42 -0500

    Adding the stats components

--- frontend/src/components/sectorStack/SectorStack.tsx
+++ frontend/src/components/sectorStack/SectorStack.tsx
@@ -1,44 +1,52 @@
-import {  useEffect, useState } from 'react';
-import { BarChart, Bar, ResponsiveContainer, XAxis, YAxis, Tooltip, Legend } from 'recharts'
-import { getStats } from '../../lib/api';
-
+import { useEffect, useState } from "react";
+import {
+    BarChart,
+    Bar,
+    ResponsiveContainer,
+    XAxis,
+    YAxis,
+    Tooltip,
+    Legend,
+} from "recharts";
+import { getStats } from "../../lib/api";
 
 function formatData(data: Data) {
-   return data.map((e) => {
-    if (e.name.length > 4) {
-      e.name = e.name.slice(0, 4)+"..."
-    }
-    return e; 
-  }).filter(e => e.count > 0)
+    return data
+        .map((e) => {
+            if (e.name.length > 4) {
+                e.name = e.name.slice(0, 4) + "...";
+            }
+            return e;
+        })
+        .filter((e) => e.count > 0);
 }
-
 
 type Data = {
-  name: string;
-  count: number;
+    name: string;
+    count: number;
 }[];
 
-const SectorStats=()=> {
-  const [data, setData] = useState<Data>([]);
-  
-  useEffect(() => {
-    (async () => {
-      const res = await getStats({ year: "2023", month: "10" })
-      setData(formatData(res));
-    })()
-  }, [])
+const SectorStats = () => {
+    const [data, setData] = useState<Data>([]);
+
+    useEffect(() => {
+        (async () => {
+            const res = await getStats({ year: "2023", month: "10" });
+            setData(formatData(res));
+        })();
+    }, []);
 
     return (
-      <ResponsiveContainer width="50%" height="50%">
-        <BarChart width={150} height={40} data={data}>
-        <XAxis dataKey="name"/>
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="count" fill="#8884d8" />
-        </BarChart>
-      </ResponsiveContainer>
+        <ResponsiveContainer width="100%" height="100%">
+            <BarChart width={100} height={40} data={data}>
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="count" fill="#305A7F" />
+            </BarChart>
+        </ResponsiveContainer>
     );
-}
+};
 
-export default SectorStats
+export default SectorStats;
