9ff69beda90cc7dd9ff7380a4a59856f10a66b7c
OnjaFanS <onjarabemanantsimba@gmail.com>
2023-11-02 08:35:25 +0300

    current

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,107 +1,112 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import styles from "./Login.module.css";
 import { useAuth } from "../../hooks/auth";
+import styles from "./Login.module.css";
+import s from './Login.module.css'
+
+
 function Login() {
-    const { login } = useAuth();
-    const navigate = useNavigate();
+  const { login } = useAuth();
+  const navigate = useNavigate();
 
-    const [error, setError] = useState(false);
-    const [user, setUser] = useState({
-        email: "",
-        password: "",
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
     });
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setUser({
-            ...user,
-            [name]: value,
-        });
-    };
+  };
 
-    const handleSubmit = () => {
-        const { email, password } = user;
-        login(email, password)
-            .then(() => {
-                navigate("/");
-            })
-            .catch(() => {
-                setError(true);
-            });
-    };
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError("");
+    setLoading(true);
 
-    return (
-        <section className="flex justify-center items-center columns-12">
-            <div className={`border rounded-md mt-9 p-2' ${styles.contenair}`}>
-                <h1 className="text-center text-blue-600 font-bold pt-2 text-lg">
-                    Login
-                </h1>
-                <form
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                        handleSubmit();
-                    }}
-                    className="flex justify-center items-center flex-col"
-                >
-                    <div className="mt-4 mb-6">
-                        <label
-                            htmlFor="email"
-                            className="block mb-2 text-sm font-medium text-gray-400"
-                        >
-                            E-mail
-                        </label>
-                        <input
-                            type="email"
-                            placeholder="votre e-mail"
-                            id="email"
-                            onChange={handleChange}
-                            name="email"
-                            className=" rounded-lg p-2.5 block text-sm focus:outline-none"
-                        />
-                    </div>
-                    <div className="mt-2 mb-6">
-                        <label
-                            htmlFor="password"
-                            className="block mb-2 text-sm font-medium text-gray-400"
-                        >
-                            Mots de passe
-                        </label>
-                        <input
-                            type="password"
-                            placeholder="mots de passe"
-                            onChange={handleChange}
-                            id="password"
-                            name="password"
-                            className="rounded-lg p-2.5 block text-sm focus:outline-none"
-                        />
-                    </div>
-                    {error ? (
-                        <p className={`text-red-700 mb-3`}>
-                            Email ou mot de passe incorrect
-                        </p>
-                    ) : null}
-                    <a href="#" className={`${styles.forgot}`}>
-                        forgot password?
-                    </a>
-                    <div className="mt-4 mb-6">
-                        <button
-                            type="submit"
-                            className={`p-3 rounded-full text-sm ${styles.btn}`}
-                        >
-                            connexion
-                        </button>
-                    </div>
+    const { email, password } = user;
 
-                    <p className="text-m m-8">
-                        creez-vous un compte{" "}
-                        <Link to="/sign-up" className="text-blue-500">
-                            ici
-                        </Link>
-                    </p>
-                </form>
-            </div>
-        </section>
-    );
+    try {
+      await login(email, password);
+      navigate("/");
+    } catch (error) {
+      setError("Email ou mot de passe incorrect");
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (error) {
+      const timeout = setTimeout(() => {
+        setError("");
+      }, 5000);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [error]);
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-800">
+      <div className="w-96 p-8 bg-white rounded-lg shadow-md">
+        <h1 className="text-center text-3xl font-semibold text-blueColor mb-6">Connexion</h1>
+        <form onSubmit={handleSubmit} className="flex justify-center items-center flex-col">
+          <div className="mt-4 mb-6">
+            <label className="block mb-2 text-sm font-medium text-gray-600">E-mail</label>
+            <input
+              type="email"
+              placeholder="Votre e-mail"
+              name="email"
+              value={user.email}
+              onChange={handleChange}
+              className={`rounded-lg p-2.5 block text-sm focus:outline-none w-full ${
+                error ? "border-red-500" : "border-gray-300"
+              }`}
+            />
+          </div>
+          <div className="mt-2 mb-6">
+            <label className="block mb-2 text-sm font-medium text-gray-600">Mot de passe</label>
+            <input
+              type="password"
+              placeholder="Mot de passe"
+              name="password"
+              value={user.password}
+              onChange={handleChange}
+              className={`rounded-lg p-2.5 block text-sm focus:outline-none w-full ${
+                error ? "border-red-500" : "border-gray-300"
+              }`}
+            />
+          </div>
+          {error && <p className="text-red-600 mb-3">{error}</p>}
+          <Link to="/forgot-password" className={`${styles.forgot} text-blueColor hover:underline`}>
+            Mot de passe oublié?
+          </Link>
+          <div className="mt-4 mb-6">
+            <button
+              type="submit"
+              className={`p-3 rounded-full text-sm bg-blueColor text-white w-full transform transition-transform hover:scale-105 ${
+                s.btn
+              }`}
+              disabled={loading}
+            >
+              {loading ? "Chargement..." : "Connexion"}
+            </button>
+          </div>
+          <p className="text-m m-8">
+            Créez-vous un compte{" "}
+            <Link to="/sign-up" className="text-blue-600 hover:underline">
+              ici
+            </Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- frontend/src/pages/SignUp/SignUp.tsx
+++ frontend/src/pages/SignUp/SignUp.tsx
@@ -3,45 +3,84 @@ import { Link } from 'react-router-dom'
 import s from './Signup.module.css'
 
 function SignUp() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    repassword: ""
+  });
 
-    const [user, setUser] = useState({
-        email: "",
-        password: "",
-        repassword: ""
-    })
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target
-        setUser({
-            ...user,
-            [name]: value
-        })
-    }
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value
+    });
+  };
 
-    return (
-        <section className="flex justify-center items-center columns-12">
-            <div className={`'border rounded-md mt-9 p-2' ${s.contenair}`}>
-                <h1 className='text-center text-blue-600 font-bold pt-2 text-lg'>SignUp</h1>
-                <form action="post" className="flex justify-center items-center flex-col">
-                    <div className="mt-4 mb-6">
-                        <label htmlFor='email' className="block mb-2 text-sm font-medium text-gray-400">E-mail</label>
-                        <input type="email" placeholder="votre e-mail" onChange={handleChange} id="email" value={user.email} name='email' className={`rounded-lg p-2.5 block text-sm focus:outline-none ${s.email}`} />
-                    </div>
-                    <div className="mt-2 mb-6">
-                        <label htmlFor='password' className="block mb-2 text-sm font-medium text-gray-400">Mots de passe</label>
-                        <input type="password" placeholder="*****" value={user.password} onChange={handleChange} id="password" name='password' className={`rounded-lg p-2.5 block text-sm focus:outline-none ${s.password}`} />
-                    </div>
-                    <div className="mt-2 mb-6">
-                        <label htmlFor='repassword' className="block mb-2 text-sm font-medium text-gray-400">confirme mots de passe</label>
-                        <input type="password" placeholder="*****" value={user.repassword} onChange={handleChange} id="repassword" name='repassword' className={`rounded-lg p-2.5 block text-sm focus:outline-none ${s.password}`} />
-                    </div>
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-800">
+      <div className={`w-96 p-8 bg-white rounded-lg shadow-md ${s.container}`}>
+        <h1 className='text-center text-3xl font-semibold text-blueColor mb-6'>SignUp</h1>
+        <form onSubmit={() => {}} className="flex justify-center items-center flex-col">
+          <div className="mt-4 mb-6">
+            <label htmlFor='email' className="block mb-2 text-sm font-medium text-gray-600">E-mail</label>
+            <input
+              type="email"
+              placeholder="Votre e-mail"
+              onChange={handleChange}
+              id="email"
+              value={user.email}
+              name='email'
+              className={`rounded-lg p-2.5 block text-sm focus:outline-none w-full ${
+                s.input
+              }`}
+            />
+          </div>
+          <div className="mt-2 mb-6">
+            <label htmlFor='password' className="block mb-2 text-sm font-medium text-gray-600">Mot de passe</label>
+            <input
+              type="password"
+              placeholder="*****"
+              value={user.password}
+              onChange={handleChange}
+              id="password"
+              name='password'
+              className={`rounded-lg p-2.5 block text-sm focus:outline-none w-full ${
+                s.input
+              }`}
+            />
+          </div>
+          <div className="mt-2 mb-6">
+            <label htmlFor='repassword' className="block mb-2 text-sm font-medium text-gray-600">Confirmer le mot de passe</label>
+            <input
+              type="password"
+              placeholder="*****"
+              value={user.repassword}
+              onChange={handleChange}
+              id="repassword"
+              name='repassword'
+              className={`rounded-lg p-2.5 block text-sm focus:outline-none w-full ${
+                s.input
+              }`}
+            />
+          </div>
 
-                    <div className="mt-4 mb-6">
-                        <Link to='/login'><button type='submit' className={`p-3 rounded-full text-sm ${s.btn}`}>Creer compte</button></Link>
-                    </div>
-
-                </form>
-            </div>
-        </section>
-    );
+          <div className="mt-4 mb-6">
+            <Link to='/login'>
+              <button
+                type='submit'
+                className={`p-3 rounded-full text-sm bg-blueColor text-white w-full transform transition-transform hover:scale-105 ${
+                  s.btn
+                }`}
+              >
+                Créer un compte
+              </button>
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
+
 export default SignUp;
